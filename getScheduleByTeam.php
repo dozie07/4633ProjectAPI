346a374b9fa03f6d23169612054881b8336b606d
cfranchi <cameron.franchi3@gmail.com>
2020-11-26 11:40:40 -0500

    set variable to post

--- getScheduleByTeam.php
+++ getScheduleByTeam.php
@@ -2,13 +2,8 @@
     include './ScheduleProvider.php';
 
     $provider = new ScheduleProvider();
-    
-    //temporary variable set to string for testing
     $team1 = "team1";
-
-    //actual variable will be a $_POST or $_GET recieved from web app requester
-    //$team1 = $_POST["team1"];
-
+    $team1 = $_POST["team1"];
     $provider->getScheduleByTeam($team1);
     echo json_encode($provider->returnService()->returnMatches());
 ?>
