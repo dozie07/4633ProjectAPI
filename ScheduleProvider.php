6a14b76e25d02f8bcea087ffad7a4de2f25152cc
cfranchi <cameron.franchi3@gmail.com>
2020-12-01 20:14:04 -0500

    Update ScheduleProvider.php

--- ScheduleProvider.php
+++ ScheduleProvider.php
@@ -29,7 +29,6 @@ class ScheduleProvider implements IScheduleProvider {
         if ($getResults === false) {
             echo (sqlsrv_errors());
         }
-        echo "update7";
         while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_BOTH)) {
             $team1 = $row['HomeTeam'];
             $team2 = $row['AwayTeam'];
