aecb90973325571ff21c66cbb53f1e340df166c9
cfranchi <cameron.franchi3@gmail.com>
2020-12-01 19:51:04 -0500

    Update getScheduleByTeam.php

--- getScheduleByTeam.php
+++ getScheduleByTeam.php
@@ -1,10 +1,10 @@
 <?php
     header("Access-Control-Allow-Origin: *");
 
-    echo "fucking hello?";
     include './ScheduleProvider.php';
 
     $provider = new ScheduleProvider();
+    echo "fucking hello?";
     $team1 = $_POST["team1"];
     $team1 = "Eintracht Frankfurt";
     $provider->getScheduleByTeam($team1);
