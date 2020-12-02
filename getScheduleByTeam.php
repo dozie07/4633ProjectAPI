4992ec91e87bd8f6d498e0ece98c1f18e621e474
cfranchi <cameron.franchi3@gmail.com>
2020-12-01 20:05:05 -0500

    Update getScheduleByTeam.php

--- getScheduleByTeam.php
+++ getScheduleByTeam.php
@@ -5,7 +5,7 @@
 
     $provider = new ScheduleProvider();
     //$team1 = $_POST["team1"];
-    $team1 = "FC Bayern München";
+    $team1 = "Eintracht Frankfurt";
     $provider->getScheduleByTeam($team1);
     echo json_encode($provider->returnService()->returnMatches());
 ?>
