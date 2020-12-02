1fbdcbdb59e4efd938d2207e3a59d5c18c5f3cb7
cfranchi <cameron.franchi3@gmail.com>
2020-12-01 22:05:00 -0500

    Update getScheduleByMonth.php

--- getScheduleByMonth.php
+++ getScheduleByMonth.php
@@ -4,8 +4,8 @@
     include './ScheduleProvider.php';
 
     $provider = new ScheduleProvider();
-    //$month = $_POST["month"];
-    $month = "1";
+    $month = $_POST["month"];
+    //$month = "1";
     $provider->getScheduleByMonth($month);
     echo json_encode($provider->returnService()->returnMatches());
 ?>
